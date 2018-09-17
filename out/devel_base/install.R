4a9afab0fb2b6a8d5b8d2e259beafe7799cf7d9c
LiNk-NY <mramos09@gmail.com>
2018-09-17 14:58:37 -0400

    update yml file

--- out/devel_base/install.R
+++ out/devel_base/install.R
@@ -4,7 +4,11 @@
 if ("BiocManager" %in% rownames(installed.packages()))
 	remove.packages("BiocManager")
 
-install.packages("BiocManager", repos="https://cran.rstudio.com")
+if (TRUE)
+    devtools::install_github("Bioconductor/BiocManager")
+else
+    install.packages("BiocManager", repos="https://cran.rstudio.com")
+
 library(BiocManager)
 
 if(BiocManager::version() != "3.8"){
@@ -16,12 +20,13 @@ builtins <- c("Matrix", "KernSmooth", "mgcv")
 
 for (builtin in builtins)
     if (!suppressWarnings(require(builtin, character.only=TRUE)))
-        suppressWarnings(BiocManager::install(builtin,
-                                              version="3.8",
-                                              update=TRUE, ask=FALSE))
+        suppressWarnings(
+            BiocManager::install(builtin, version="3.8",
+            update=TRUE, ask=FALSE)
+        )
 
 
-if(FALSE){
+if (FALSE) {
 # save BiocInstaller for final run of release 3.7
 url <- "http://bioconductor.org/packages/3.8/bioc"
 
